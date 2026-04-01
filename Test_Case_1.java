b7d415376556065edf62c0dbb115fc210beffdff
BetseeNatasha <154437933+BetseeNatasha@users.noreply.github.com>
2026-04-01 21:33:06 +0530

    Add files via upload

--- Test_Case_1.java
+++ Test_Case_1.java
@@ -1,4 +1,4 @@
-package SeleniumWait;
+package Practice;
 
 import java.time.Duration;
 
@@ -9,14 +9,24 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class Test_Case_1 {
 
 	public static void main(String[] args) {
-    WebDriver driver=new ChromeDriver();
-    driver.manage().window().maximize();
-    driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
-    driver.get("https://demowebshop.tricentis.com/");
-    driver.findElement(By.linkText("Facebook")).click();
-    driver.findElement(By.linkText("Twitter")).click();
-    driver.findElement(By.linkText("YouTube")).click();
-    driver.findElement(By.name("search_query")).sendKeys("Monica Tamil Song");    
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+		driver.navigate()
+				.to("https://icehrm-open.gamonoid.com/login.php?next=P2c9YWRtaW4mbj1kYXNoYm9hcmQmbT1hZG1pbl9BZG1pbg");
+		driver.findElement(By.id("username")).sendKeys("Betsee");
+		driver.findElement(By.id("password")).sendKeys("betsee");
+		driver.findElement(By.xpath("//button[@class='btn btn-info btn-medium w-100 text-uppercase']")).click();
+		driver.findElement(By.id("username")).sendKeys("admin");
+		driver.findElement(By.id("password")).sendKeys("admin");
+		driver.findElement(By.xpath("//button[@class='btn btn-info btn-medium w-100 text-uppercase']")).click();
+		driver.findElement(By.xpath("//span[text()='Later']")).click();
+		driver.findElement(By.xpath("//span[text()='Employees']")).click();
+		driver.findElement(By.linkText("Skills")).click();
+		driver.findElement(By.className("ant-modal-close")).click();
+		driver.findElement(By.className("ant-table-cell")).isDisplayed();
+        driver.findElement(By.xpath("//span[text()='IceHrm ']")).click();
+		driver.findElement(By.linkText("Sign out")).click();
 	}
 
 }
